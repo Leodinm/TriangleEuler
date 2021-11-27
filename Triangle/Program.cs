ac1052d1b053831f29028eb3befbc74d1d8e8630
Leonidasdim <Leondiasdimakis2@gmail.com>
2021-11-28 01:59:30 +0200

    Add project files.

---
+++ Triangle/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/* https://projecteuler.net/problem=67 */
+namespace Triangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Tree mytree = new Tree();
+            Console.WriteLine("Sum best path is {0}", mytree.SumTriangle());
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ Triangle/Tree.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Triangle
+{
+    class Tree
+    {
+        public   int  SumTriangle()
+
+
+        {
+            string workingDirectory = Environment.CurrentDirectory;
+            string path= Path.Combine(Directory.GetParent(workingDirectory).Parent.FullName, @"Triangle\", @"Triangle.txt");
+            string text = File.ReadAllText(path);
+
+            
+
+            var Triangle = text.Split(new char[] { '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries)
+       
+                .Select(line => line.Trim()
+                    .Split(' ')
+                    .Select(s => int.Parse(s.Trim()))
+                    .ToArray())
+                .ToArray();
+
+            return SumTree(Triangle);
+
+        }
+
+        //DP  bottom-up  
+        // for each element, check both 
+        // elements just below the number 
+        // and below right to the number 
+        // add the maximum of them to it 
+
+
+
+        private int SumTree(int[][] Triangle)
+        {
+           
+            for (int i = Triangle.Length - 2; i >=0; i--)
+            {
+                for (int j=0;j<= Triangle[i].Length-1;j++)
+                {
+
+                    if (Triangle[i + 1][j] > Triangle[i + 1][ j + 1])
+
+                        Triangle[i][ j] += Triangle[i + 1][ j];
+
+                    else
+                        Triangle[i][ j] += Triangle[i + 1][ j + 1];
+
+
+
+                }
+            }
+            return Triangle[0][0];
+
+        }
+
+     
+
+       
+
+    }
+}
+
+
